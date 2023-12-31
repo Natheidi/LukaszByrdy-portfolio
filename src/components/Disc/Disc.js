70e6542ffd55d22dff097d5c11ca107157fa46e4
nataliabyrdy <nataliabyrdy@onet.pl>
2023-12-31 18:26:01 +0100

    changes - disc

--- src/components/Disc/Disc.js
+++ src/components/Disc/Disc.js
@@ -11,7 +11,7 @@ const Disc = () => {
                 </h3>
                 <div className='cd-flex'>
                     <div className='cd-container'>
-                        <img className='cd-img' src={require('../../assets/img/cd.jpg')} alt='cd'/>
+                        <img className='cd-img' src={require('../../assets/img/cd.jpg')} alt='cd' />
                         <img className='cd-img' src={require('../../assets/img/cd2.jpg')} alt='cd' />
                     </div>
                     <div className='text-c'>
@@ -30,21 +30,21 @@ const Disc = () => {
                     <div className='audio-small-section'>
                         <h4 className='title-playlist'>{t('disc.playlist')}</h4>
                         <h4 className='title-playlist2'>WOLFGANG AMADEUSZ  MOZART – CONCERTO KV 488</h4>
-                        <audio controls className='audio-clips'>
-                            <source src={require('../../assets/audio/01_MOZART_A-dur_Koncert1.mp3')} type="audio/mpeg" />
 
-                        </audio>
                         <p className='audio-text'>01 MOZART</p>
                         <audio controls className='audio-clips'>
-                            <source src={require('../../assets/audio/02_MOZART_A-dur_Koncert2.mp3')} type="audio/mpeg" />
-
+                            <source src={require('../../assets/audio/01_MOZART_A-dur_Koncert1.mp3')} type="audio/mpeg" />
                         </audio>
+
                         <p className='audio-text'>02 MOZART</p>
                         <audio controls className='audio-clips'>
-                            <source src={require('../../assets/audio/03_MOZART_A-dur_Koncert3.mp3')} type="audio/mpeg" />
-
+                            <source src={require('../../assets/audio/02_MOZART_A-dur_Koncert2.mp3')} type="audio/mpeg" />
                         </audio>
+
                         <p className='audio-text'>03 MOZART</p>
+                        <audio controls className='audio-clips'>
+                            <source src={require('../../assets/audio/03_MOZART_A-dur_Koncert3.mp3')} type="audio/mpeg" />
+                        </audio>
                     </div>
                     <hr className='line'></hr>
                     <div className='audio-small-section'>
