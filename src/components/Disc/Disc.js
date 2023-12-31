fbac917563b856cd2ababcdc2c32847cdba0d410
nataliabyrdy <nataliabyrdy@onet.pl>
2023-12-31 18:43:13 +0100

    Audio files

--- src/components/Disc/Disc.js
+++ src/components/Disc/Disc.js
@@ -31,17 +31,17 @@ const Disc = () => {
                         <h4 className='title-playlist'>{t('disc.playlist')}</h4>
                         <h4 className='title-playlist2'>WOLFGANG AMADEUSZ  MOZART – CONCERTO KV 488</h4>
 
-                        <p className='audio-text'>01 MOZART</p>
+                        <p className='audio-text'>MOZART I</p>
                         <audio controls className='audio-clips'>
                             <source src={require('../../assets/audio/01_MOZART_A-dur_Koncert1.mp3')} type="audio/mpeg" />
                         </audio>
 
-                        <p className='audio-text'>02 MOZART</p>
+                        <p className='audio-text'>MOZART II</p>
                         <audio controls className='audio-clips'>
                             <source src={require('../../assets/audio/02_MOZART_A-dur_Koncert2.mp3')} type="audio/mpeg" />
                         </audio>
 
-                        <p className='audio-text'>03 MOZART</p>
+                        <p className='audio-text'>MOZART III</p>
                         <audio controls className='audio-clips'>
                             <source src={require('../../assets/audio/03_MOZART_A-dur_Koncert3.mp3')} type="audio/mpeg" />
                         </audio>
