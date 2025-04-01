01e2ead9ef3779d94df20e57cf9ae04606b8026d
nataliabyrdy <nataliabyrdy@onet.pl>
2025-04-01 22:14:46 +0200

    youtube

--- src/components/Swiper/Swiper.js
+++ src/components/Swiper/Swiper.js
@@ -29,7 +29,20 @@ const SwiperComponent = () => {
                 <p className='video-text'>Szymanowski - Tantris the Clown from Masques op.34</p>
             </SwiperSlide>
             <SwiperSlide>
-                <iframe className='yt-video' src="https://www.youtube.com/embed/3ZPMISEIEGg?si=qGU7CRirStvjad5i" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <iframe className='yt-video' src="https://www.youtube.com/watch?v=3ZPMISEIEGg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <p className='video-text'>Mussorgsky - Pictures at an Exhibition</p>
+            </SwiperSlide>
+
+            <SwiperSlide>
+                <iframe className='yt-video' src="https://www.youtube.com/embed/orf7l6EXics?si=mG445-NTe0opA2RS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <p className='video-text'>Szymanowski - Don Juan Serenade from Masques op.34</p>
+            </SwiperSlide>
+            <SwiperSlide>
+                <iframe className='yt-video' src="https://www.youtube.com/embed/G4D7UJywdP0?si=3o8RnceNB9l739p0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <p className='video-text'>Szymanowski - Tantris the Clown from Masques op.34</p>
+            </SwiperSlide>
+            <SwiperSlide>
+                <iframe className='yt-video' src="https://www.youtube.com/watch?v=3ZPMISEIEGg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                 <p className='video-text'>Mussorgsky - Pictures at an Exhibition</p>
             </SwiperSlide>
             ...
