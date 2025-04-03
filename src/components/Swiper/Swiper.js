46b69962d6bfe08b77db0ee1f2c5e951ece54c8f
nataliabyrdy <nataliabyrdy@onet.pl>
2025-04-03 18:41:25 +0200

    links

--- src/components/Swiper/Swiper.js
+++ src/components/Swiper/Swiper.js
@@ -20,19 +20,8 @@ const SwiperComponent = () => {
         >
 
             
-            <SwiperSlide>
-                <iframe className='yt-video' src="https://www.youtube.com/embed/orf7l6EXics?si=mG445-NTe0opA2RS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                <p className='video-text'>Szymanowski - Don Juan Serenade from Masques op. 34</p>
-            </SwiperSlide>
-            <SwiperSlide>
-                <iframe className='yt-video' src="https://www.youtube.com/embed/G4D7UJywdP0?si=3o8RnceNB9l739p0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                <p className='video-text'>Szymanowski - Tantris the Clown from Masques op. 34</p>
-            </SwiperSlide>
-            <SwiperSlide>
-                <iframe className='yt-video' src="https://www.youtube.com/embed/3ZPMISEIEGg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                <p className='video-text'>Mussorgsky - Pictures at an Exhibition</p>
-            </SwiperSlide>
-
+     
+           
             <SwiperSlide>
                 <iframe className='yt-video' src="https://www.youtube.com/embed/DWZ0Ls8JQaU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                 <p className='video-text'>Chopin - Andante Spianato and Grande Polonaise Brillante in E flat major Op. 22</p>
@@ -47,12 +36,21 @@ const SwiperComponent = () => {
             </SwiperSlide>
 
             <SwiperSlide>
-                <iframe className='yt-video' src="https://www.youtube.com/embed/NDTbjbL5ymA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                <p className='video-text'>Chopin Concerts in Łazienki Królewskie2021</p>
+                <iframe className='yt-video' src="https://www.youtube.com/embed/Df6HL3Sibjs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <p className='video-text'>Chopin - Nocturne in C minor Op. 48 No. 1</p>
             </SwiperSlide>
             <SwiperSlide>
-                <iframe className='yt-video' src="https://www.youtube.com/embed/NDTbjbL5ymA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <iframe className='yt-video' src="https://www.youtube.com/embed/ME_CCzzyQFc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                 <p className='video-text'>Saturday Chopin Recitals in Żelazowa Wola</p>
+            </SwiperSlide>
+
+            <SwiperSlide>
+                <iframe className='yt-video' src="https://www.youtube.com/embed/orf7l6EXics?si=mG445-NTe0opA2RS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <p className='video-text'>Szymanowski - Don Juan Serenade from Masques op. 34</p>
+            </SwiperSlide>
+            <SwiperSlide>
+                <iframe className='yt-video' src="https://www.youtube.com/embed/G4D7UJywdP0?si=3o8RnceNB9l739p0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <p className='video-text'>Szymanowski - Tantris the Clown from Masques op. 34</p>
             </SwiperSlide>
 
 
