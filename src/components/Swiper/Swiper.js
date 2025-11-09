dd4e6fb9fc2888f530661c9a728865e9e93291cb
Natheidi <nataliabyrdy@onet.pl>
2025-11-09 21:10:46 +0100

    links

--- src/components/Swiper/Swiper.js
+++ src/components/Swiper/Swiper.js
@@ -39,12 +39,13 @@ const SwiperComponent = () => {
                 <p className="video-text">
                     Chopin - Andante Spianato and Grande Polonaise Brillante in E flat major Op. 22
                 </p>
-            </SwiperSlide><SwiperSlide>
+            </SwiperSlide>
+            <SwiperSlide>
                 <div className="aspect-video w-full">
                     <iframe
                         className="yt-video"
-                        src="https://www.youtube.com/embed/DWZ0Ls8JQaU?rel=0&modestbranding=1&playsinline=1"
-                        title="Chopin - Andante Spianato and Grande Polonaise Brillante, Op. 22"
+                        src="https://www.youtube.com/embed/0cGek8uQjXo?rel=0&modestbranding=1&playsinline=1"
+                        title="Chopin - Scherzo in C sharp minor Op. 39"
                         frameBorder="0"
                         loading="lazy"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
@@ -52,36 +53,87 @@ const SwiperComponent = () => {
                         referrerPolicy="strict-origin-when-cross-origin"
                     />
                 </div>
-
-                <p className="video-text">
-                    Chopin - Andante Spianato and Grande Polonaise Brillante in E flat major Op. 22
-                </p>
-            </SwiperSlide>
-            <SwiperSlide>
-                <iframe className='yt-video' src="https://www.youtube.com/embed/0cGek8uQjXo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                <p className='video-text'>Chopin - Scherzo in C sharp minor Op. 39</p>
-            </SwiperSlide>
-            <SwiperSlide>
-                <iframe className='yt-video' src="https://www.youtube.com/embed/OZxePyoeCQ4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                <p className='video-text'>Chopin - Etude in F major Op. 10 No. 8</p>
+                <p className="video-text">Chopin - Scherzo in C sharp minor Op. 39</p>
             </SwiperSlide>
 
             <SwiperSlide>
-                <iframe className='yt-video' src="https://www.youtube.com/embed/Df6HL3Sibjs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                <p className='video-text'>Chopin - Nocturne in C minor Op. 48 No. 1</p>
-            </SwiperSlide>
-            <SwiperSlide>
-                <iframe className='yt-video' src="https://www.youtube.com/embed/ME_CCzzyQFc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                <p className='video-text'>Saturday Chopin Recitals in Żelazowa Wola</p>
+                <div className="aspect-video w-full">
+                    <iframe
+                        className="yt-video"
+                        src="https://www.youtube.com/embed/OZxePyoeCQ4?rel=0&modestbranding=1&playsinline=1"
+                        title="Chopin - Etude in F major Op. 10 No. 8"
+                        frameBorder="0"
+                        loading="lazy"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowFullScreen
+                        referrerPolicy="strict-origin-when-cross-origin"
+                    />
+                </div>
+                <p className="video-text">Chopin - Etude in F major Op. 10 No. 8</p>
             </SwiperSlide>
 
             <SwiperSlide>
-                <iframe className='yt-video' src="https://www.youtube.com/embed/orf7l6EXics?si=mG445-NTe0opA2RS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                <p className='video-text'>Szymanowski - Don Juan Serenade from Masques op. 34</p>
+                <div className="aspect-video w-full">
+                    <iframe
+                        className="yt-video"
+                        src="https://www.youtube.com/embed/Df6HL3Sibjs?rel=0&modestbranding=1&playsinline=1"
+                        title="Chopin - Nocturne in C minor Op. 48 No. 1"
+                        frameBorder="0"
+                        loading="lazy"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowFullScreen
+                        referrerPolicy="strict-origin-when-cross-origin"
+                    />
+                </div>
+                <p className="video-text">Chopin - Nocturne in C minor Op. 48 No. 1</p>
             </SwiperSlide>
+
             <SwiperSlide>
-                <iframe className='yt-video' src="https://www.youtube.com/embed/G4D7UJywdP0?si=3o8RnceNB9l739p0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                <p className='video-text'>Szymanowski - Tantris the Clown from Masques op. 34</p>
+                <div className="aspect-video w-full">
+                    <iframe
+                        className="yt-video"
+                        src="https://www.youtube.com/embed/ME_CCzzyQFc?rel=0&modestbranding=1&playsinline=1"
+                        title="Saturday Chopin Recitals in Żelazowa Wola"
+                        frameBorder="0"
+                        loading="lazy"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowFullScreen
+                        referrerPolicy="strict-origin-when-cross-origin"
+                    />
+                </div>
+                <p className="video-text">Saturday Chopin Recitals in Żelazowa Wola</p>
+            </SwiperSlide>
+
+            <SwiperSlide>
+                <div className="aspect-video w-full">
+                    <iframe
+                        className="yt-video"
+                        src="https://www.youtube.com/embed/orf7l6EXics?rel=0&modestbranding=1&playsinline=1"
+                        title="Szymanowski - Don Juan Serenade from Masques op. 34"
+                        frameBorder="0"
+                        loading="lazy"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowFullScreen
+                        referrerPolicy="strict-origin-when-cross-origin"
+                    />
+                </div>
+                <p className="video-text">Szymanowski - Don Juan Serenade from Masques op. 34</p>
+            </SwiperSlide>
+
+            <SwiperSlide>
+                <div className="aspect-video w-full">
+                    <iframe
+                        className="yt-video"
+                        src="https://www.youtube.com/embed/G4D7UJywdP0?rel=0&modestbranding=1&playsinline=1"
+                        title="Szymanowski - Tantris the Clown from Masques op. 34"
+                        frameBorder="0"
+                        loading="lazy"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowFullScreen
+                        referrerPolicy="strict-origin-when-cross-origin"
+                    />
+                </div>
+                <p className="video-text">Szymanowski - Tantris the Clown from Masques op. 34</p>
             </SwiperSlide>
 
 
